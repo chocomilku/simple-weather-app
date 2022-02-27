c761542b1af5e0ecd960a2316973b8a790b562ec
chocomilku <andrei.berja078@gmail.com>
2022-02-27 21:52:47 +0800

    implemented query for coordinates

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -6,16 +6,26 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<owmStruct>
 ) {
+	const params: queryStruct = req.query;
 	try {
-		res.status(200).json(await api());
+		res.status(200).json(await api(params.lat, params.lon));
 	} catch (err: any) {
 		res.status(err.response.status).json(err.response.data);
 	}
 }
 
-const api = async (): Promise<owmStruct> => {
+const api = async (lat?: number, lon?: number): Promise<owmStruct> => {
 	const result = await axios.get(
-		`${process.env.BASE_API}?lat=14.53&lon=120.98&exclude=${process.env.APICONFIG_EXCLUDE}&units=${process.env.APICONFIG_UNITS}&lang=${process.env.APICONFIG_LANG}&appid=${process.env.KEY}`
+		`${process.env.BASE_API}?${lat ? `lat=${lat}` : ""}&${
+			lon ? `lon=${lon}` : ""
+		}&exclude=${process.env.APICONFIG_EXCLUDE}&units=${
+			process.env.APICONFIG_UNITS
+		}&lang=${process.env.APICONFIG_LANG}&appid=${process.env.KEY}`
 	);
 	return result.data;
 };
+
+interface queryStruct {
+	lat?: number;
+	lon?: number;
+}
